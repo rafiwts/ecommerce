36eaae7b6919cc11ab1415da703384bcbad33c05
rafiwts <rafiwts@gmail.com>
2023-11-21 23:44:32 +0100

    Add new html and script

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,11 @@
+// change the rotation of the arrow for categories tab upon clicking
+console.log('Script is running!');
+
 function dropdownBox() {
     var dropdownList = document.getElementById("dropdownList");
     dropdownList.classList.toggle("show");
 
-    var arrow = document.querySelector('.tabs img');
+    var arrow = document.querySelector('.tab-bar img');
     arrow.style.transform = dropdownList.classList.contains('show') ? 'rotate(270deg)' : 'rotate(90deg)';
 }
 
@@ -16,8 +19,33 @@ window.onclick = function (event) {
                 openDropdown.classList.remove('show');
 
                 // Reset the arrow rotation
-                document.querySelector('.tabs img').style.transform = 'rotate(90deg)';
+                document.querySelector('.tab-bar img').style.transform = 'rotate(90deg)';
             }
         }
     }
 }
+
+// resize sidebar depending on the width of the site
+window.addEventListener('resize', updateSidebarWidth);
+
+function updateSidebarWidth() {
+    var tabs = document.querySelector('.tabs');
+    var sidebar = document.getElementById('sidebar');
+
+    if (tabs) {
+        var marginLeft = window.getComputedStyle(tabs).getPropertyValue('margin-left');
+        sidebar.style.width = marginLeft;
+
+        // Check if the max-width is reached
+        var maxWidth = parseInt(window.getComputedStyle(sidebar).getPropertyValue('max-width'));
+        var adjustedMaxWidth = 250;
+
+        if (parseInt(marginLeft) > adjustedMaxWidth) {
+            sidebar.style.maxWidth = adjustedMaxWidth + 'px';
+        } else {
+            sidebar.style.maxWidth = '';
+        }
+    }
+}
+
+updateSidebarWidth();
