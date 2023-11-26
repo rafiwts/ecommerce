6a001ee94d1b7da38c69be28947f184bed0423aa
rafiwts <rafiwts@gmail.com>
2023-11-26 23:26:43 +0100

    Add script to dropbox buttons and change names

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,48 +1,64 @@
-// change the rotation of the arrow for categories tab upon clicking
-function dropdownBox() {
-    var dropdownList = document.getElementById("dropdownList");
-    dropdownList.classList.toggle("show");
+// change the rotation of the arrow for categories tab upon clicking for 3 buttons:
+function dropdownCategory() {
+    var dropdownList = document.getElementById("dropdownListCategory");
+    dropdownList.classList.toggle("show-categories");
 
-    var arrow = document.querySelector('.arrow');
-    arrow.style.transform = dropdownList.classList.contains('show') ? 'rotate(270deg)' : 'rotate(90deg)';
+    var arrow = document.querySelector('.category-arrow');
+    arrow.style.transform = dropdownList.classList.contains('show-categories') ? 'rotate(270deg)' : 'rotate(90deg)';
+}
+
+function dropdownLanguage() {
+    var dropdownList = document.getElementById("dropdownListLanguage");
+    dropdownList.classList.toggle("show-languages");
+
+    var arrow = document.querySelector('.language-arrow');
+    arrow.style.transform = dropdownList.classList.contains('show-languages') ? 'rotate(270deg)' : 'rotate(90deg)';
+}
+
+function dropdownCurrency() {
+    var dropdownList = document.getElementById("dropdownListCurrency");
+    dropdownList.classList.toggle("show-currencies");
+
+    var arrow = document.querySelector('.currency-arrow');
+    arrow.style.transform = dropdownList.classList.contains('show-currencies') ? 'rotate(270deg)' : 'rotate(90deg)';
 }
 
 window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-categories");
+    // Check for category dropdown
+    if (!event.target.matches('.category-dropbtn')) {
+        var categoryDropdowns = document.getElementsByClassName("categories-dropdown");
         var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-
-                // Reset the arrow rotation
-                document.querySelector('.tab-bar img').style.transform = 'rotate(90deg)';
+        for (i = 0; i < categoryDropdowns.length; i++) {
+            var openDropdown = categoryDropdowns[i];
+            if (openDropdown.classList.contains('show-categories')) {
+                openDropdown.classList.remove('show-categories');
+                document.querySelector('.category-dropbtn img').style.transform = 'rotate(90deg)';
             }
         }
     }
-}
 
-// change the rotation of the arrow for categories tab upon clicking
-function dropdownBoxHelp() {
-    var dropdownList = document.getElementById("dropdownListHelp");
-    dropdownList.classList.toggle("dupsko");
+    // Check for language dropdown
+    if (!event.target.matches('.language-dropbtn')) {
+        var languageDropdowns = document.getElementsByClassName("languages-dropdown");
+        var j;
+        for (j = 0; j < languageDropdowns.length; j++) {
+            var openDropdown = languageDropdowns[j];
+            if (openDropdown.classList.contains('show-languages')) {
+                openDropdown.classList.remove('show-languages');
+                document.querySelector('.language-dropbtn img').style.transform = 'rotate(90deg)';
+            }
+        }
+    }
 
-    var arrow = document.querySelector('.arrow-help');
-    arrow.style.transform = dropdownList.classList.contains('dupsko') ? 'rotate(270deg)' : 'rotate(90deg)';
-}
-
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn-help')) {
-        var dropdowns = document.getElementsByClassName("dropdown-categories-help");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('dupsko')) {
-                openDropdown.classList.remove('dupsko');
-
-                // Reset the arrow rotation
-                document.querySelector('.right-help-bar img').style.transform = 'rotate(90deg)';
+    // Check for currency dropdown
+    if (!event.target.matches('.currency-dropbtn')) {
+        var currencyDropdowns = document.getElementsByClassName("currencies-dropdown");
+        var k;
+        for (k = 0; k < currencyDropdowns.length; k++) {
+            var openDropdown = currencyDropdowns[k];
+            if (openDropdown.classList.contains('show-currencies')) {
+                openDropdown.classList.remove('show-currencies');
+                document.querySelector('.currency-dropbtn img').style.transform = 'rotate(90deg)';
             }
         }
     }
