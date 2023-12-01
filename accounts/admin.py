db470b37176a643b37a7881f56f9b61c10d97215
rafiwts <rafiwts@gmail.com>
2023-12-01 23:40:17 +0100

    Update account models and admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,87 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Account, AccountType, User, UserAddress
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "get_account_type",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+        "id",
+    )
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "username",
+                    "email",
+                    "password",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "account",
+                    "groups",
+                    "user_permissions",
+                ]
+            },
+        )
+    ]
+    ordering = ["is_superuser", "is_staff", "username"]
+
+    def get_account_type(self, obj):
+        return obj.account.account_type if obj.account else None
+
+    get_account_type.short_description = "Account Type"
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        "get_full_name",
+        "get_full_address",
+        "get_account_name",
+        "image",
+        "created_at",
+    )
+    ordering = ["last_name", "first_name"]
+
+    @admin.display(description="Full name")
+    def get_full_name(self, obj):
+        return f"{obj.last_name}, {obj.first_name}"
+
+    def get_full_address(self, obj):
+        if obj.address:
+            address = obj.address
+            return (
+                f"{address.street}, "
+                f"{address.zip_code}, "
+                f"{address.city}, "
+                f"{address.country}"
+            )
+        else:
+            return None
+
+    get_full_address.short_description = "Adress"
+
+    def get_account_name(self, obj):
+        return obj.account_type.name if obj.account_type else None
+
+    get_account_name.short_description = "Account type"
+
+
+@admin.register(AccountType)
+class AccountTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "discount_value")
+    ordering = ["name"]
+
+
+@admin.register(UserAddress)
+class UserAddressAdmin(admin.ModelAdmin):
+    list_display = ("street", "zip_code", "city", "state", "country")
+    ordering = ["country", "city", "street", "zip_code"]
--- accounts/models.py
+++ accounts/models.py
@@ -4,44 +4,84 @@ from django.db import models
 from .handlers import image_directory_path
 
 
+# TODO: check image path and apply login to a website
 class User(AbstractUser):
-    account = models.ForeignKey(
+    account = models.OneToOneField(
         "Account", on_delete=models.CASCADE, null=True, blank=True, related_name="users"
     )
 
     def __str__(self):
         return self.username
 
+    class Meta:
+        db_table = "users"
 
-# TODO: think what you can add to account type and apply models to admin site
+
+# TODO: think what you can add to account type
 class AccountType(models.Model):
     name = models.CharField(max_length=50, unique=True)
     discount_value = models.PositiveIntegerField(
-        default=0, help_text="Discount value when 'discount' is True"
+        default=0, help_text="Discount value for a user", verbose_name="discount"
     )
+    # TODO: add duration and function that checks if you still have valid account type
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "account_types"
 
 
-# add verbose names and see admin fields
 class Account(models.Model):
-    address = models.ForeignKey(
-        "UserAddress", on_delete=models.CASCADE, null=True, blank=True
+    address = models.OneToOneField(
+        "UserAddress", on_delete=models.SET_NULL, null=True, blank=True
     )
     account_type = models.ForeignKey(
-        AccountType, on_delete=models.CASCADE, null=True, blank=True
+        AccountType, on_delete=models.SET_NULL, null=True, blank=True
     )
-    first_name = models.CharField(max_length=100, null=False)
-    last_name = models.CharField(max_length=100, null=False)
-    image = models.ImageField(upload_to=image_directory_path, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    first_name = models.CharField(max_length=100, null=False, verbose_name="name")
+    last_name = models.CharField(max_length=100, null=False, verbose_name="surname")
+    image = models.ImageField(
+        upload_to=image_directory_path,
+        null=True,
+        blank=True,
+        verbose_name="profile picture",
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=True,
+        blank=True,
+        help_text="When the account was created",
+        verbose_name="created",
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        null=True,
+        blank=True,
+        help_text="When the account was updated",
+        verbose_name="updated",
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+    class Meta:
+        db_table = "accounts"
+        ordering = ["-created_at", "last_name"]
+
 
 class UserAddress(models.Model):
     street = models.CharField(max_length=50)
-    zip_code = models.CharField(max_length=15)
+    zip_code = models.CharField(max_length=15, verbose_name="zip code")
     city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
+    state = models.CharField(
+        max_length=50, help_text="The name of the province in which you live"
+    )
     country = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.country}"
+
+    class Meta:
+        db_table = "addresses"
+        verbose_name_plural = "User addresses"
