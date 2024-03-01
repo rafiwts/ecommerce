dacc67c3c1a0ecb544f26ff16e781cfd9daad97f
rafiwts <rafiwts@gmail.com>
2024-03-01 22:36:38 +0100

    Add tests for views and users

--- accounts/tests/fixtures.py
+++ accounts/tests/fixtures.py
@@ -1,6 +1,25 @@
 import pytest
 from django.test import Client
 
+from accounts.models import User
+
+
+@pytest.fixture(scope="session")
+def custom_users(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        users = [
+            User.objects.create_user(
+                username="mako", email="mako@gmail.com", password="Stefan13!"
+            ),
+            User.objects.create_user(
+                username="tako", email="tako@gmail.com", password="August13!"
+            ),
+            User.objects.create_user(
+                username="bako", email="bako@gmail.com", password="Zygmunt13!"
+            ),
+        ]
+        yield users
+
 
 @pytest.fixture
 def client(db):
--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,10 +1,101 @@
+import os
+
 import pytest
 from django.urls import reverse
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 @pytest.mark.django_db
-def test_views(client):
-    url = reverse("home")
-    response = client.get(url)
+class TestView:
+    @pytest.mark.parametrize(
+        "template_name",
+        {
+            "home",
+            "account:register",
+            "account:login",
+        },
+    )
+    def test_views_with_no_login_required(self, client, template_name):
+        url = reverse(template_name)
+        response = client.get(url)
 
-    assert response.status_code == 200
+        assert response.status_code == 200
+
+    @pytest.mark.parametrize(
+        "template_name, login_user, password",
+        {
+            ("account:profile-view", "mako", "Stefan13!"),
+            ("account:edit-account", "tako", "August13!"),
+            ("account:edit-address", "bako", "Zygmunt13!"),
+            (
+                "account:change-password",
+                "rafiwts",
+                os.environ.get("SUPERUSER_PASSWORD"),
+            ),
+            ("account:shipping-address-add", "tako", "August13!"),
+        },
+    )
+    def test_views_with_login_required(
+        self, client, template_name, login_user, password, custom_users
+    ):
+        client.login(username=login_user, password=password)
+        url = reverse(template_name, kwargs={"username": login_user})
+
+        response = client.get(url)
+
+        assert response.status_code == 200
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "login_user, password",
+    [
+        ("rafiwts", os.environ.get("SUPERUSER_PASSWORD")),
+        ("rafiwts@ecommerce.com", os.environ.get("SUPERUSER_PASSWORD")),
+    ],
+)
+def test_login_superuser_with_username_and_email(client, login_user, password):
+    url = reverse("account:login")
+    data = {"username": login_user, "password": password}
+    response = client.post(url, data=data)
+
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "login_user, password",
+    [
+        ("mako", "Stefan13!"),
+        ("mako@gmail.com", "Stefan13!"),
+        ("tako", "August13!"),
+        ("tako@gmail.com", "August13!"),
+        ("bako", "Zygmunt13!"),
+        ("bako@gmail.com", "Zygmunt13!"),
+    ],
+)
+def test_login_users_with_username_and_email(
+    client, login_user, password, custom_users
+):
+    url = reverse("account:login")
+    data = {"username": login_user, "password": password}
+    response = client.post(url, data=data)
+
+    assert response.status_code == 302
+
+
+@pytest.mark.django_db
+def test_logout_user(client, custom_users):
+    login_url = reverse("account:login")
+    data = {"username": "mako", "password": "Stefan13!"}
+
+    response = client.post(login_url, data=data)
+
+    assert response.status_code == 302
+
+    logout_url = reverse("account:logout")
+    response = client.get(logout_url)
+
+    assert response.status_code == 302
